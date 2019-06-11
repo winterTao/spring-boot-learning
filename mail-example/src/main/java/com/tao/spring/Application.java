c5a91f994d229fd5a30d7325a88fba874c9902c0
tao.dong <tao.dong@ruisdata.com>
2019-06-11 22:19:51 +0800

    Update : 添加 mail 例子

---
+++ mail-example/src/main/java/com/tao/spring/Application.java
@@ -0,0 +1,17 @@
+package com.tao.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DongTao
+ * @since 2019-06-11
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
---
+++ mail-example/src/test/java/com/tao/spring/ApplicationTests.java
@@ -0,0 +1,33 @@
+package com.tao.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author DongTao
+ * @since 2019-06-11
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("2956345646@qq.com");
+        message.setTo("2956345646@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+
+        mailSender.send(message);
+    }
+
+}
