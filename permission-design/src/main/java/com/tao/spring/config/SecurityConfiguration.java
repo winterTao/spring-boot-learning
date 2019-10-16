822eef13e36cf3946ed4c39aaadf40c37e9891fe
tao.dong <tao.dong@ruisdata.com>
2019-10-16 11:29:03 +0800

    Update : content type

--- permission-design/src/main/java/com/tao/spring/config/SecurityConfiguration.java
+++ permission-design/src/main/java/com/tao/spring/config/SecurityConfiguration.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tao.spring.dto.RespBean;
 import com.tao.spring.service.UserService;
 import com.tao.spring.util.UserUtils;
+import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.CredentialsExpiredException;
@@ -19,9 +24,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 /**
  * @author DongTao
@@ -73,68 +82,89 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
-                    @Override
-                    public <O extends FilterSecurityInterceptor> O postProcess(O o) {
-                        o.setSecurityMetadataSource(customMetadataSource);
-                        o.setAccessDecisionManager(urlAccessDecisionManager);
-                        return o;
-                    }
-                })
+                .withObjectPostProcessor(getObjectPostProcessor())
                 .and()
                 .formLogin()
                 .usernameParameter("username")
                 .passwordParameter("password")
-                .failureHandler((req, resp, e) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    RespBean respBean = null;
-                    if (e instanceof BadCredentialsException ||
-                            e instanceof UsernameNotFoundException) {
-                        respBean = RespBean.error("账户名或者密码输入错误!");
-                    } else if (e instanceof LockedException) {
-                        respBean = RespBean.error("账户被锁定，请联系管理员!");
-                    } else if (e instanceof CredentialsExpiredException) {
-                        respBean = RespBean.error("密码过期，请联系管理员!");
-                    } else if (e instanceof AccountExpiredException) {
-                        respBean = RespBean.error("账户过期，请联系管理员!");
-                    } else if (e instanceof DisabledException) {
-                        respBean = RespBean.error("账户被禁用，请联系管理员!");
-                    } else {
-                        respBean = RespBean.error("登录失败!");
-                    }
-                    resp.setStatus(401);
-                    ObjectMapper om = new ObjectMapper();
-                    PrintWriter out = resp.getWriter();
-                    out.write(om.writeValueAsString(respBean));
-                    out.flush();
-                    out.close();
-                })
-                .successHandler((req, resp, auth) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    RespBean respBean = RespBean.ok("登录成功!", UserUtils.getCurrentUser());
-                    ObjectMapper om = new ObjectMapper();
-                    PrintWriter out = resp.getWriter();
-                    out.write(om.writeValueAsString(respBean));
-                    out.flush();
-                    out.close();
-                })
+                .failureHandler(getAuthenticationFailureHandler())
+                .successHandler(getAuthenticationSuccessHandler())
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessHandler((req, resp, authentication) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    RespBean respBean = RespBean.ok("注销成功!");
-                    ObjectMapper om = new ObjectMapper();
-                    PrintWriter out = resp.getWriter();
-                    out.write(om.writeValueAsString(respBean));
-                    out.flush();
-                    out.close();
-                })
+                .logoutSuccessHandler(getLogoutSuccessHandler())
                 .permitAll()
                 .and()
                 .csrf()
                 .disable()
                 .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler);
+    }
+
+    private ObjectPostProcessor<FilterSecurityInterceptor> getObjectPostProcessor() {
+        return new ObjectPostProcessor<FilterSecurityInterceptor>() {
+            @Override
+            public <O extends FilterSecurityInterceptor> O postProcess(O o) {
+                o.setSecurityMetadataSource(customMetadataSource);
+                o.setAccessDecisionManager(urlAccessDecisionManager);
+                return o;
+            }
+        };
+    }
+
+    private AuthenticationSuccessHandler getAuthenticationSuccessHandler() {
+        return (req, resp, auth) -> {
+            resp.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            RespBean respBean = RespBean.ok("登录成功!", UserUtils.getCurrentUser());
+            ObjectMapper om = new ObjectMapper();
+            PrintWriter out = resp.getWriter();
+            out.write(om.writeValueAsString(respBean));
+            out.flush();
+            out.close();
+        };
+    }
+
+    private AuthenticationFailureHandler getAuthenticationFailureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest req,
+                    HttpServletResponse resp, AuthenticationException e)
+                    throws IOException, ServletException {
+                resp.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+                RespBean respBean;
+                if (e instanceof BadCredentialsException
+                        || e instanceof UsernameNotFoundException) {
+                    respBean = RespBean.error("账户名或者密码输入错误!");
+                } else if (e instanceof LockedException) {
+                    respBean = RespBean.error("账户被锁定，请联系管理员!");
+                } else if (e instanceof CredentialsExpiredException) {
+                    respBean = RespBean.error("密码过期，请联系管理员!");
+                } else if (e instanceof AccountExpiredException) {
+                    respBean = RespBean.error("账户过期，请联系管理员!");
+                } else if (e instanceof DisabledException) {
+                    respBean = RespBean.error("账户被禁用，请联系管理员!");
+                } else {
+                    respBean = RespBean.error("登录失败!");
+                }
+                resp.setStatus(401);
+                ObjectMapper om = new ObjectMapper();
+                PrintWriter out = resp.getWriter();
+                out.write(om.writeValueAsString(respBean));
+                out.flush();
+                out.close();
+            }
+        };
+    }
+
+    private LogoutSuccessHandler getLogoutSuccessHandler() {
+        return (req, resp, authentication) -> {
+            resp.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            RespBean respBean = RespBean.ok("注销成功!");
+            ObjectMapper om = new ObjectMapper();
+            PrintWriter out = resp.getWriter();
+            out.write(om.writeValueAsString(respBean));
+            out.flush();
+            out.close();
+        };
     }
 }
