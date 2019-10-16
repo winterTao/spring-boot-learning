420957c1514741f20e6eaed829319eb6a7335b24
tao.dong <tao.dong@ruisdata.com>
2019-10-16 14:31:06 +0800

    Update : 添加过期时间测试

--- spring-redis/src/test/java/com/tao/spring/util/RedisUtilsTest.java
+++ spring-redis/src/test/java/com/tao/spring/util/RedisUtilsTest.java
@@ -1,9 +1,11 @@
 package com.tao.spring.util;
 
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -17,10 +19,25 @@ public class RedisUtilsTest {
     @Autowired
     private RedisUtils redisUtils;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     public void test() {
         redisUtils.set("foo", "bar");
         String value = redisUtils.get("foo");
         System.out.println(value);
+    }
+
+    @Test
+    public void test2() {
+        redisTemplate.opsForValue().set("time", "123", 60, TimeUnit.SECONDS);
+        System.out.println(redisTemplate.opsForValue().get("time"));
+        System.out.println(redisTemplate.getExpire("time"));
+
+        redisTemplate.opsForValue().set("time2", "1234");
+        System.out.println(redisTemplate.opsForValue().get("time2"));
+        System.out.println(redisTemplate.getExpire("time2"));
+
     }
 }
