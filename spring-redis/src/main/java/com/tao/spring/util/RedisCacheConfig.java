549cfb6f3de4a412ab075e3469cdc3ecf8ebcc71
tao.dong <tao.dong@ruisdata.com>
2019-11-27 16:09:48 +0800

    Update : 添加redis cache manager

---
+++ spring-redis/src/main/java/com/tao/spring/util/RedisCacheConfig.java
@@ -0,0 +1,78 @@
+package com.tao.spring.util;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+/**
+ * @author DongTao
+ * @since 2019-11-22
+ */
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public KeyGenerator simpleKeyGenerator() {
+        return (o, method, objects) -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(o.getClass().getSimpleName());
+            stringBuilder.append(".");
+            stringBuilder.append(method.getName());
+            stringBuilder.append("[");
+            for (Object obj : objects) {
+                stringBuilder.append(obj.toString());
+            }
+            stringBuilder.append("]");
+
+            return stringBuilder.toString();
+        };
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+                // 默认策略，未配置的 key 会使用这个
+                this.getRedisCacheConfigurationWithTtl(600),
+                // 指定 key 策略
+                this.getRedisCacheConfigurationMap()
+        );
+    }
+
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> map = new HashMap<>(2);
+        map.put("test1", this.getRedisCacheConfigurationWithTtl(30));
+        map.put("test2", this.getRedisCacheConfigurationWithTtl(100));
+        return map;
+    }
+
+    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(
+                Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        cacheConfig = cacheConfig.serializeValuesWith(RedisSerializationContext
+                .SerializationPair
+                .fromSerializer(jackson2JsonRedisSerializer))
+                .entryTtl(Duration.ofSeconds(seconds));
+
+        return cacheConfig;
+    }
+}
